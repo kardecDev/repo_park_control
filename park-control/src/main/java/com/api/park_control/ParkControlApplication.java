c746d428b647b73af891de88e5471e1fe8dcbfb4
Allan Kardec Neves Alves <kardec.dev@gmail.com>
2024-09-03 00:58:33 -0300

    feat: Correção devContainer, Inicialização do projeto park-control.

---
+++ park-control/src/main/java/com/api/park_control/ParkControlApplication.java
@@ -0,0 +1,13 @@
+package com.api.park_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkControlApplication.class, args);
+	}
+
+}
